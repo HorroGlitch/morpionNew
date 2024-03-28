d1d6f59e51ba8c95d870abcc8b58a588d9ea4647
HorroGlitch <lverot@edenschool.fr>
2024-03-28 16:31:21 +0100

    "x" & "o" condition player add

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let squares = document.querySelectorAll(".square");
 
-setupCounter(document.querySelector('#counter'))
+let currentPlayer = 1;
+let count = 0;
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click',()=>{
+
+    if (count === 8) {
+      setTimeout(() => {
+        alert("Fin de partie")
+      }, 10);
+      
+    }
+
+    if (squares[i].textContent === "" && currentPlayer === 1) {
+
+      squares[i].textContent = "x";
+      currentPlayer = 2;
+      console.log(currentPlayer);
+      count++
+
+    } else if(squares[i].textContent === "" && currentPlayer === 2) {
+
+      squares[i].textContent = "o";
+      currentPlayer = 1;
+      console.log(currentPlayer);
+      count++;
+
+    }
+
+  })
+  
+}
